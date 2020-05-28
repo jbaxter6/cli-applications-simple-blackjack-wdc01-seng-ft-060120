0dbfdeda946cfd9fa849791c2c12e501d0a23fd2
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-28 05:01:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -69,7 +69,7 @@ def runner
   # code runner here
   welcome
   initial_round
-    if initial_round <= 21
+    until initial_round > 21
       sum = hit?(initial_round)
       display_card_total(sum)
     end
